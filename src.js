8a5131a6cd173dd69f73b9e4aa3113dfbda3acbf
Brandon Nguyen <bnguyenlps@gmail.com>
2021-02-24 14:30:25 -0800

    Add a function that allows us to add pieces to board

--- src.js
+++ src.js
@@ -1,4 +1,14 @@
 
 var board = [ ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ];
 
+// console.log(`  ${board[0]} | ${board[1]} | ${board[2]} \n ------------ \n  ${board[3]} | ${board[4]} | ${board[5]} \n ------------ \n  ${board[6]} | ${board[7]} | ${board[8]} `);
+
+const addToBoard = (place, mark) => {
+  board[place] = mark;
+}
+
+addToBoard(0, 'X');
+addToBoard(1, 'X');
+addToBoard(2, 'X');
+
 console.log(`  ${board[0]} | ${board[1]} | ${board[2]} \n ------------ \n  ${board[3]} | ${board[4]} | ${board[5]} \n ------------ \n  ${board[6]} | ${board[7]} | ${board[8]} `);
