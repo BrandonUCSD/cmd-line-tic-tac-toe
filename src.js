b8e8fee6ce3948a9c6846e18f302ef3cfb54ea3b
Brandon Nguyen <bnguyenlps@gmail.com>
2021-02-24 14:26:37 -0800

    create repo and establish what board looks like

---
+++ src.js
@@ -0,0 +1,4 @@
+
+var board = [ ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' , ' ' ];
+
+console.log(`  ${board[0]} | ${board[1]} | ${board[2]} \n ------------ \n  ${board[3]} | ${board[4]} | ${board[5]} \n ------------ \n  ${board[6]} | ${board[7]} | ${board[8]} `);
